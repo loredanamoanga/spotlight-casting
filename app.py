fef8b1592bf262b109b086f3d929b230961020ec
Loredana Moanga <loredana.moanga@container-solutions.com>
2020-03-05 15:18:21 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,42 @@
+import os
+from flask import Flask, request, abort, jsonify
+from flask_migrate import Migrate
+from flask_moment import Moment
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+
+import dateutil.parser
+import babel
+from flask import (
+    Flask,
+    render_template,
+    request,
+    flash,
+    redirect,
+    url_for)
+from flask_moment import Moment
+from flask_sqlalchemy import SQLAlchemy
+import logging
+from logging import Formatter, FileHandler
+from sqlalchemy.exc import SQLAlchemyError
+# from forms import *
+from flask_migrate import Migrate
+
+
+def create_app(test_config=None):
+  # create and configure the app
+  app = Flask(__name__)
+  app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+  moment = Moment(app)
+  app.config.from_object('config')
+  db = SQLAlchemy(app)
+  migrate = Migrate(app, db)
+
+  CORS(app)
+
+  return app
+
+APP = create_app()
+
+if __name__ == '__main__':
+    APP.run(host='0.0.0.0', port=8080, debug=True)
---
+++ models.py
@@ -0,0 +1,116 @@
+import os
+from sqlalchemy import Column, String, Integer, ForeignKey
+from flask_sqlalchemy import SQLAlchemy
+import json
+
+database_path = 'postgresql://tomaswingord:tomasw87@localhost:5432/casting'
+
+db = SQLAlchemy()
+db_id = Column(Integer().with_variant(Integer, "sqlite"), primary_key=True)
+db_name = Column(String(80), unique=True)
+
+
+def db_drop_and_create_all():
+    db.drop_all()
+    db.create_all()
+
+
+class Actor(db.Model):
+    id = db_id
+    name = db_name
+
+    def format(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'age': self.age,
+            'gender': self.gender
+        }
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def __repr__(self):
+        return json.dumps(self.format())
+
+
+class Movie(db.Model):
+    id = db_id
+    name = db_name
+
+    def format(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'release_date': self.release_date
+        }
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def __repr__(self):
+        return json.dumps(self.format())
+
+
+class Masterpieces(db.Model):
+    id = db_id,
+    actor_id = Column('id', ForeignKey('actor.id'),
+                      primary_key=True, autoincrement='ignore_fk')
+    movie_id = Column('id', ForeignKey('movie.id'),
+                      primary_key=True, autoincrement='ignore_fk')
+    title = Column(String(80), unique=True)
+
+    def format(self):
+        return {
+            'id': self.id,
+            'actor_id': self.actor_id,
+            'movie_id': self.movie_id,
+        }
+
+    '''
+    insert()
+        inserts a new model into a database
+        the model must have a unique name
+        the model must have a unique id or null id
+    '''
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    '''delete()
+      deletes a new model into a database
+      the model must exist in the database
+      '''
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    '''
+        update()
+            updates a new model into a database
+            the model must exist in the database
+        '''
+
+    def update(self):
+        db.session.commit()
+
+    def __repr__(self):
+        return json.dumps(self.format())
